bd8d61a7d4e31d6448fea1e258d0a6adef1be28e
HummyMolecula <dragonitdeveloper@gmail.com>
2022-02-22 18:14:00 +0200

    add simple list

--- string.py
+++ string.py
@@ -5,5 +5,9 @@ n = int(input("Enter step: "))
 word = word[:i:n].capitalize()
 print(word)
 
-sym = int(input("Enter symbol: "))
-print("At index:", word.find(sym))
+sym = input("Enter symbol: ")
+print("At index: ", word.find(sym))
+
+sym = input("Enter new symbol: ")
+list = word.split(sym)
+print("List: ", list)
